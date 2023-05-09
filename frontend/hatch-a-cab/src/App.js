93c9c69eb46a48be8def0b5c9efdf50910a01441
Akshit Batra <akshit.batra@thoughtworks.com>
2023-05-09 20:34:37 +0530

    RR-3 | Akshit, Nallu | Add. BookingConfirmation with static data

--- frontend/hatch-a-cab/src/App.js
+++ frontend/hatch-a-cab/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.scss';
+import logo from "./logo.svg";
+import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from './pages/LandingPage';
-import Login from './pages/Login';
-import Booking from './pages/Booking';
-import BookingConfirmation from './pages/Bookingconfirmation';
-import SignUp from './pages/Signup';
-import ErrorPage from './pages/ErrorPage';
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Booking from "./pages/Booking";
+import BookingConfirmation from "./pages/Bookingconfirmation";
+import SignUp from "./pages/Signup";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
+  const ride = {
+    dropLocation: "Delhi",
+    pickUpLocation: "Chennai",
+    time: "2022-04-22 10:34:23",
+    status: "confirmed",
+  };
+  
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="login"  element={<Login />} />
-          <Route path="signup" element={<SignUp />} />
-          <Route path="booking" element={<LandingPage />} />
-          <Route path="booking-confirmation" element={<BookingConfirmation />} />
-          <Route path="*" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="booking" element={<Booking />} />
+        <Route
+          path="booking-confirmation"
+          element={<BookingConfirmation ride={ride} />}
+        />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/hatch-a-cab/src/pages/Bookingconfirmation.js
+++ frontend/hatch-a-cab/src/pages/Bookingconfirmation.js
@@ -1,9 +1,31 @@
-import React from "react"
+import React from "react";
+import moment from "moment";
+import "./../styles/pages/BookingConfirmation.scss";
 
-const BookingConfirmation = (props) => {
-    return (
-        <div>BookingConfirmation Page will render here.</div>
-    )
-}
+const BookingConfirmation = ({ ride }) => {
+  const date = moment(ride.date).format("ddd DD MMM, hh:mm A");
+  const checkStatus = () => {
+    if (ride.status === "confirmed") {
+      return <h5>Ride Booked Successful</h5>;
+    } else return <h5>Ride Booking Failed</h5>;
+  };
+  return (
+    <div className="container">
+      {checkStatus()}
+      <div>
+        <div>
+          <strong>Pickup :</strong> {ride.pickUpLocation}
+        </div>
+        <div>
+          <strong>Destination :</strong> {ride.pickUpLocation}
+        </div>
+        <hr />
+        <div>
+          <strong>Time :</strong> {date}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default BookingConfirmation;
