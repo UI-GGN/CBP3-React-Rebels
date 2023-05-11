f3c83295a7bbcb10f1586d43d07fa7ca767132da
nallammaim <69379808+nallammaim@users.noreply.github.com>
2023-05-11 12:20:34 +0530

    RR- 3|Akshit Nallu|add request info

--- frontend/hatch-a-cab/src/pages/Bookingconfirmation.js
+++ frontend/hatch-a-cab/src/pages/Bookingconfirmation.js
@@ -1,29 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 import "./../styles/pages/BookingConfirmation.scss";
 
-const BookingConfirmation = ({ ride }) => {
-  const date = moment(ride.date).format("ddd DD MMM, hh:mm A");
-  const checkStatus = () => {
+const BookingConfirmation = ({ rides }) => {
+  const convertDate = (date) => moment(date).format("ddd DD MMM, hh:mm A");
+
+  const [bookingId,setBookingId] = useState();
+
+  const [selectedCab, setSelectedCab] = useState({
+    bookingId:1,
+    dropLocation: "address1",
+    pickUpLocation: "address2",
+    city:'Chennai',
+    area:'T.Nagar',
+    time: '2022-04-22 10:34:23',
+    status: "confirmed",
+  });
+
+  const checkStatus = (ride) => {
     if (ride.status === "confirmed") {
       return <h5>Ride Booked Successful</h5>;
     } else return <h5>Ride Booking Failed</h5>;
   };
-  return (
-    <div className="container">
-      {checkStatus()}
+
+  const selectCab = (bookingId) => {
+    const cab =rides.find(item => item.bookingId == bookingId)
+    setSelectedCab(cab);
+  }
+  const showDetails = () => {
+    return (
       <div>
+        Details
+        <div>{selectedCab?.area}</div>
+        <div>{selectedCab?.date}</div>
         <div>
-          <strong>Pickup :</strong> {ride.pickUpLocation}
+          <strong>Pickup :</strong> {selectedCab?.pickUpLocation}
         </div>
         <div>
-          <strong>Destination :</strong> {ride.pickUpLocation}
+          <strong>Destination :</strong> {selectedCab?.pickUpLocation}
         </div>
         <hr />
         <div>
-          <strong>Time :</strong> {date}
+          <strong>Time :</strong> {convertDate(selectedCab.time)}
         </div>
       </div>
+    )
+  }
+  return (
+    <div>
+      <div>
+        {rides.map(ride => (
+          <div className="container" key={ride.bookingId}>
+            {checkStatus(ride)}
+            <div>
+              <div onClick={()=>selectCab(ride.bookingId)}>
+                <strong>Pickup :</strong> {ride.pickUpLocation}
+              </div>
+              <div>
+                <strong>Destination :</strong> {ride.pickUpLocation}
+              </div>
+              <hr />
+              <div>
+                <strong>Time :</strong> {convertDate(ride.time)}
+              </div>
+            </div>
+          </div>
+        )
+
+        )}
+      </div>
+      {selectedCab !== undefined ? showDetails() : <></>}
     </div>
   );
 };
