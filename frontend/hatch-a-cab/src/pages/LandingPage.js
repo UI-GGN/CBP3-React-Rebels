639b65aa8fd4a9229534c4a5ba8fb7cb5ebfeaad
DHRUSOM <dhruva.som@thoughtworks.com>
2023-05-11 21:39:43 +0530

    RR-2|[Dhruva/Riya]|Add validation

--- frontend/hatch-a-cab/src/pages/LandingPage.js
+++ frontend/hatch-a-cab/src/pages/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../styles/pages/LandingPage.scss"
+import "../styles/pages/LandingPage.scss";
 
 function LandingPage() {
   const initialValues = {
@@ -13,18 +13,100 @@ function LandingPage() {
   };
 
   const [values, setValues] = useState(initialValues);
+  const [cityError, setCityError] = useState("");
+  const [areaError, setAreaError] = useState("");
+  const [line1Error, setLine1Error] = useState("");
+  const [line2Error, setLine2Error] = useState("");
+  const [pincodeError, setPincodeError] = useState("");
+  const [pickupTimeError, setPickupTimeError] = useState("");
+  const [fromDateError, setFromDateError] = useState("");
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setValues({
-      ...values,
-      [name]: value,
+    const trimedValue = value.trim();
+    setValues((prevState) => {
+      return { ...prevState, [name]: trimedValue };
     });
+  };
+
+  const cityInputBlurHandler = () => {
+    if (!values.city.trim().length > 0) {
+      setCityError("City is mandatory.");
+      return;
+    }
+    setCityError("");
+  };
+
+  const areaInputBlurHandler = () => {
+    if (!values.area.trim().length > 0) {
+      setAreaError("Area is mandatory.");
+      return;
+    }
+    setAreaError("");
+  };
+
+  const line1InputBlurHandler = () => {
+    if (!values.line1.trim().length > 0) {
+      setLine1Error("Line 1 is mandatory.");
+      return;
+    }
+    setLine1Error("");
+  };
+
+  const line2InputBlurHandler = () => {};
+
+  const pincodeInputBlurHandler = () => {
+    if (!values.pincode.trim().length > 0) {
+      setPincodeError("Pincode is mandatory.");
+      return;
+    }
+    setPincodeError("");
+  };
+
+  const pickupTimeInputBlurHandler = () => {
+    if (values.pickupTime) {
+      var today = new Date();
+      var time = today.getHours() + 2 + ":" + today.getMinutes();
+      console.log(values.pickupTime)
+      console.log(time)
+      if (values.pickupTime < time) {
+        setPickupTimeError(
+          "Pickup time atleast should be 2 hrs later from now."
+        );
+        return;
+      }
+    }
+    setPickupTimeError("");
+  };
+
+  const fromDateInputBlurHandler = () => {
+    if (values.fromDate) {
+      var today = new Date();
+      var formDate = values.fromDate
+        .split("-")
+        .reduce((total = 0, num) => parseInt(total) + parseInt(num));
+      var date = today.getFullYear() + (today.getMonth() + 1) + today.getDate();
+      if (formDate < date) {
+        setFromDateError("From date should be in future.");
+        return;
+      }
+    }
+    setFromDateError("");
+  };
+
+  const autoValidateForm = () => {
+    cityInputBlurHandler();
+    areaInputBlurHandler();
+    line1InputBlurHandler();
+    line2InputBlurHandler();
+    pincodeInputBlurHandler();
+    pickupTimeInputBlurHandler();
+    fromDateInputBlurHandler();
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(values);
+    autoValidateForm();
   };
 
   return (
@@ -35,82 +117,108 @@ function LandingPage() {
           <div className="form-group field">
             <label className="label">City</label>
             <select
-                name="city"
-                class="form-select"
-                value={values.city}
-                onChange={handleInputChange}
-              >
-                <option value="Gurugram">Gurugram</option>
-                <option value="Delhi">Delhi</option>
+              name="city"
+              className="form-select"
+              value={values.city}
+              onChange={handleInputChange}
+              onBlur={cityInputBlurHandler}
+            >
+              <option value="Gurugram">Gurugram</option>
+              <option value="Delhi">Delhi</option>
             </select>
+            {cityError.length > 1 && (
+              <div className="text-danger">{cityError}</div>
+            )}
           </div>
           <div className="form-group field">
             <label className="label">Sector/Area</label>
             <input
-                type="text"
-                name="area"
-                className="form-control"
-                value={values.area}
-                onChange={handleInputChange}
-                required
+              type="text"
+              name="area"
+              className="form-control"
+              value={values.area}
+              onChange={handleInputChange}
+              onBlur={areaInputBlurHandler}
             />
+            {areaError.length > 1 && (
+              <div className="text-danger">{areaError}</div>
+            )}
           </div>
           <div className="form-group field">
             <label>Address</label>
             <div>
-                <label className="sub-label">Line 1</label>
-                <input
-                    type="text"
-                    name="line1"
-                    className="form-control"
-                    value={values.line1}
-                    placeholder="Enter Line 1"
-                    onChange={handleInputChange}
-                    required
-                />
-                <label className="sub-label">Line 2</label>
-                <input
-                    type="text"
-                    name="line2"
-                    className="form-control"
-                    value={values.line2}
-                    placeholder="Enter Line 2"
-                    onChange={handleInputChange}
-                    required
-                />
-                <label className="sub-label">Pincode</label>
-                <input
-                    type="text"
-                    name="pincode"
-                    className="form-control"
-                    value={values.pincode}
-                    placeholder="Enter Pincode"
-                    onChange={handleInputChange}
-                    required
-                />
-                </div>
+              <label className="sub-label">Line 1</label>
+              <input
+                type="text"
+                name="line1"
+                className="form-control"
+                value={values.line1}
+                placeholder="Enter Line 1"
+                onChange={handleInputChange}
+                onBlur={line1InputBlurHandler}
+              />
+              {line1Error.length > 1 && (
+                <div className="text-danger">{line1Error}</div>
+              )}
+              <label className="sub-label">Line 2</label>
+              <input
+                type="text"
+                name="line2"
+                className="form-control"
+                value={values.line2}
+                placeholder="Enter Line 2"
+                onChange={handleInputChange}
+                onBlur={line2InputBlurHandler}
+              />
+              {line2Error.length > 1 && (
+                <div className="text-danger">{line2Error}</div>
+              )}
+              <label className="sub-label">Pincode</label>
+              <input
+                type="text"
+                name="pincode"
+                className="form-control"
+                value={values.pincode}
+                placeholder="Enter Pincode"
+                onChange={handleInputChange}
+                onBlur={pincodeInputBlurHandler}
+              />
+              {pincodeError.length > 1 && (
+                <div className="text-danger">{pincodeError}</div>
+              )}
+            </div>
           </div>
           <div className="form-group field">
             <label className="label">Pick Up Time</label>
             <input
-                type="time"
-                name="pickupTime"
-                className="form-control"
-                value={values.pickupTime}
-                onChange={handleInputChange}
+              type="time"
+              name="pickupTime"
+              className="form-control"
+              value={values.pickupTime}
+              onChange={handleInputChange}
+              onBlur={pickupTimeInputBlurHandler}
             />
+            {pickupTimeError.length > 1 && (
+              <div className="text-danger">{pickupTimeError}</div>
+            )}
           </div>
           <div className="form-group field">
             <label className="label">Starting From</label>
             <input
-                type="date"
-                name="fromDate"
-                className="form-control"
-                value={values.fromDate}
-                onChange={handleInputChange}
-              />
+              type="date"
+              name="fromDate"
+              className="form-control"
+              value={values.fromDate}
+              onChange={handleInputChange}
+              onBlur={fromDateInputBlurHandler}
+            />
+            {fromDateError.length > 1 && (
+              <div className="text-danger">{fromDateError}</div>
+            )}
           </div>
-          <button className="btn btn-secondary submit-button" type="submit">Raise Request</button>
+          <button className="btn btn-secondary submit-button" type="submit">
+            Raise Request
+          </button>
         </form>
       </div>
     </div>
