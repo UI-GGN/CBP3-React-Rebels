a7ac5c21984a68fcf85f4604b42d0d2c42ee750a
DHRUSOM <dhruva.som@thoughtworks.com>
2023-05-22 22:04:00 +0530

    RR-2 |Dhruva, Soumya| Validating.eslint-config

--- frontend/hatch-a-cab/src/pages/Booking.js
+++ frontend/hatch-a-cab/src/pages/Booking.js
@@ -1,9 +1,7 @@
-import React from "react"
+import React, { useContext } from "react";
 
 const Booking = (props) => {
-    return (
-        <div>Booking Page will render here.</div>
-    )
-}
+  return <div>Booking Page will render here.</div>;
+};
 
 export default Booking;
