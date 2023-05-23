d0ab173da7a3eea66e38ffc826849677b548f1ed
hariomsinha7 <hariom.sinha@thoughtworks.com>
2023-05-23 14:46:52 +0530

    test2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,41 +1,49 @@
-import React, { useState } from "react"
-import { Link } from "react-router-dom"
-import axios from "axios"
-import "../styles/pages/Login.scss"
-import Form from "../components/Form"
-import { useNavigate } from "react-router-dom"
-import "../styles/components/Form.scss";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import '../styles/pages/Login.scss';
+import Form from '../components/Form';
+import { useNavigate } from 'react-router-dom';
+import '../styles/components/Form.scss';
 
-const Login = props => {
-  const [error, setError] = useState(null)
-          const navigate = useNavigate()
+const Login = (props) => {
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const handleLogin = async (email, password) => {
     try {
-      const response = await axios.post("http://localhost:3001/login", { email, password })
-      navigate("/landing")
+      const response = await axios.post('http://localhost:3001/login', {
+        email,
+        password,
+      });
+      navigate('/landing');
     } catch (error) {
-      setError("An error occurred while Login. Please try again later.")
+      setError('An error occurred while Login. Please try again later.');
     }
-  }
-  const handleError = error => {
-    setError(error)
-  }
+  };
+  const handleError = (error) => {
+    setError(error);
+  };
 
   return (
-   <div>
-        <h1 className="login-container">Hatch-A-Cab</h1>
-    <div className="login-container">
-      <h2>Employee Login</h2>
-      <Form onFieldClick={handleError} onSubmitForm={handleLogin} buttonText="Login" showSignUpFields={false} />
-      {error && <p>{error}</p>}
-      <p>
-        New User?
-        <Link to="/signup"> Signup </Link>
-      </p>
+    <div>
+      <h1 className="login-container">Hatch-A-Cab</h1>
+      <div className="login-container">
+        <h2>Employee Login</h2>
+        <Form
+          onFieldClick={handleError}
+          onSubmitForm={handleLogin}
+          buttonText="Login"
+          showSignUpFields={false}
+        />
+        {error && <p>{error}</p>}
+        <p>
+          New User?
+          <Link to="/signup"> Signup </Link>
+        </p>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
