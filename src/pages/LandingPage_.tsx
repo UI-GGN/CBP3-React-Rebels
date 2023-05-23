6236468a53a0e28fc3a28e43a86396f8fd77935d
DHRUSOM <dhruva.som@thoughtworks.com>
2023-05-23 19:09:00 +0530

    RR-2 |Dhruva, Akshit, Gunjan| Added theme properties

---
+++ src/pages/LandingPage_.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import '../styles/pages/LandingPage_.scss';
+import { useNavigate } from 'react-router-dom';
+
+const LandingPage_ = (props) => {
+  const navigate = useNavigate();
+
+  const sectionCards = [
+    {
+      id: 1,
+      label: 'Add a Cab',
+      icon: '',
+      subLabel: 'Raise a request for a Cab.',
+      redirectLink: '/booking',
+    },
+    {
+      id: 2,
+      label: 'Cab Requests',
+      icon: '',
+      subLabel: 'Check all previous cab request records.',
+      redirectLink: '/booking-confirmation',
+    },
+  ];
+
+  const handleCardClick = (section) => {
+    navigate(section?.redirectLink);
+  };
+
+  return (
+    <div className="flex justify-end">
+      <button className="bg-tw_primary hover:bg-primary text-white font-inter font-bold py-2 px-4 rounded">
+        Book a cab
+      </button>
+    </div>
+    // <div className="landing-page-wrapper">
+    //     <div className="section-cards-wrapper">
+    //         {
+    //             sectionCards.map(sectionCard=>(
+    //                 <div className="section-card" key={sectionCard?.id} onClick={()=>handleCardClick(sectionCard)}>
+    //                     <div className="label title">{sectionCard?.label}</div>
+    //                     <div className="label">{sectionCard?.subLabel}</div>
+    //                 </div>
+    //             ))
+    //         }
+    //     </div>
+    // </div>
+  );
+};
+
+export default LandingPage_;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.html',
-    './src/**/*.js',
-  ],
+  content: ['./src/**/*.html', './src/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tw_dark: '#cc850a',
+        tw_primary: '#003d4f',
+        tw_secondary: '#f2617a',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        bitter: ['Bitter', 'sans-serif'],
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
-
+};
