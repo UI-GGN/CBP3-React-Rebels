a48d1ce2fffcb17a4227469ce332694913eda11d
gunjan.agarwal <gunjan.agarwal@thoughtworks.com>
2023-06-07 20:45:58 +0530

    [Gunjan/Akshit] Refactor. Cabrequest Component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.html', './src/**/*.js', './src/**/*.tsx'],
+  content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.tsx"],
   theme: {
     extend: {
       colors: {
-        tw_dark: '#cc850a',
-        tw_primary: '#003d4f',
-        tw_secondary: '#f2617a',
+        tw_dark: "#cc850a",
+        tw_primary: "#003d4f",
+        tw_secondary: "#f2617a"
       },
       fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-        bitter: ['Bitter', 'sans-serif'],
-      },
-    },
+        inter: ["Inter", "sans-serif"],
+        bitter: ["Bitter", "sans-serif"]
+      }
+    }
   },
   variants: {},
-  plugins: [],
+  plugins: []
 };
