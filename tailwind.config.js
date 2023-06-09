e94bd259deedbb0714468b850144be5d6fef4907
gunjan.agarwal <gunjan.agarwal@thoughtworks.com>
2023-06-09 18:55:14 +0530

    [Gunjan/Akshit] Fix. CSS Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'tw-blue': '#003d4f',
-        'tw-pink': '#f2617a',
+        tw_yellow: '#cc850a',
+        tw_blue: '#003d4f',
+        tw_pink: '#f2617a',
+        tw_saphire_blue: '#47a1ad',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        bitter: ['Bitter', 'sans-serif'],
       },
     },
   },
