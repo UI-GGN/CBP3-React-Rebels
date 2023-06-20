450d98193ef29839710051c67d8238ad72cf2c36
gunjan.agarwal <gunjan.agarwal@thoughtworks.com>
2023-06-20 20:29:49 +0530

    [Gunjan/Akshit] Fix. conflict issues

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import "./App.scss";
-import "tailwindcss/tailwind.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AddCab from "./pages/AddCab";
-import Login from "./pages/Login";
-import BookingConfirmation from "./pages/Bookingconfirmation";
-import SignUp from "./pages/Signup";
-import LandingPage from "./pages/LandingPage";
-import CabRequest from "./components/CabRequest/CabRequest.tsx";
+import './App.scss';
+import 'tailwindcss/tailwind.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AddCab from './pages/AddCab';
+import Login from './pages/Login';
+import BookingConfirmation from './pages/Bookingconfirmation';
+import SignUp from './pages/Signup';
+import LandingPage from './pages/LandingPage';
+import CabRequest from './components/CabRequest/CabRequest.tsx';
 
 function App() {
   const rides = [
     {
       bookingId: 1,
-      dropLocation: "address1",
-      pickUpLocation: "address2",
-      city: "Chennai",
-      area: "T.Nagar",
-      time: "2022-04-22 10:34:23",
-      status: "confirmed"
+      dropLocation: 'address1',
+      pickUpLocation: 'address2',
+      city: 'Chennai',
+      area: 'T.Nagar',
+      time: '2022-04-22 10:34:23',
+      status: 'confirmed',
     },
     {
       bookingId: 2,
-      dropLocation: "Delhi",
-      pickUpLocation: "Chennai",
-      time: "2022-04-22 10:34:23",
-      city: "Chennai",
-      area: "T.Nagar",
-      status: "confirmed"
+      dropLocation: 'Delhi',
+      pickUpLocation: 'Chennai',
+      time: '2022-04-22 10:34:23',
+      city: 'Chennai',
+      area: 'T.Nagar',
+      status: 'confirmed',
     },
     {
       bookingId: 3,
-      dropLocation: "Delhi",
-      pickUpLocation: "Chennai",
-      time: "2022-04-22 10:34:23",
-      city: "Chennai",
-      area: "T.Nagar",
-      status: "confirmed"
-    }
+      dropLocation: 'Delhi',
+      pickUpLocation: 'Chennai',
+      time: '2022-04-22 10:34:23',
+      city: 'Chennai',
+      area: 'T.Nagar',
+      status: 'confirmed',
+    },
   ];
 
   return (
@@ -47,9 +47,12 @@ function App() {
         <Route path="signup" element={<SignUp />} />
         <Route path="booking" element={<AddCab />} />
         <Route path="home" element={<LandingPage />} />
-        <Route path="booking-confirmation" element={<BookingConfirmation rides={rides} />} />
+        <Route
+          path="booking-confirmation"
+          element={<BookingConfirmation rides={rides} />}
+        />
         <Route path="*" element={<Login />} />
-        <Route path="/cab" element={<CabRequest />} />
+        <Route path="/cab-request" element={<CabRequest />} />
       </Routes>
     </BrowserRouter>
   );
