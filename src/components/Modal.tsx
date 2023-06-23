bf1aa0926ab93a15eb48ad90d5f1e1ed70229b12
Soumya Samanvaya <soumya.samanvaya@thoughtworks.com>
2023-06-23 13:50:00 +0530

    Soumya | Refactor. Modal Component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -15,16 +15,17 @@ export const Modal = ({ showModal, children, onClose }: ModalProps) => {
   return (
     <div className={showModal ? 'visible' : 'invisible'}>
       <div className="fixed inset-0 bg-gray-700 opacity-80"></div>
-      <div className="fixed mx-auto inset-40 w-[60rem] p-10 bg-white overflow-hidden">
-        <div className="p-2">
-          <div className="flex flex-row-reverse">
+      <div className="fixed inset-40 flex items-start justify-center">
+        <div className="bg-white w-[60rem] h-[80vh] p-10 overflow-hidden">
+          <div className="flex justify-end mb-4">
             <RxCross2
               className="cursor-pointer text-lg"
               size="1.4rem"
               onClick={onClose}
             />
           </div>
-          <div className="h-[60rem] max-h-[30rem] overflow-y-auto">
+
+          <div className="h-[40rem] max-h-[40rem] overflow-y-auto">
             {children}
           </div>
         </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,6 +8,7 @@ import '../styles/components/Form.scss';
 import Layout from '../components/template/Layout';
 import Logo from '../components/Logo';
 import { Modal } from '../components/Modal';
+import Button from '../components/Button';
 
 const Login = (props) => {
   const [error, setError] = useState(null);
@@ -105,7 +106,21 @@ const Login = (props) => {
           amet...' Lorem Ipsum is simply dummy text of the printing and
           typesetting industry. Lorem Ipsum has been the industry's standard
           dummy text ever since the 1500s, when an unknown printer took a galley
-          of type and scraum passage, and
+          of type and scraum passage, and some? There are many variations of
+          passages of Lorem Ipsum available, but the majority have suffered
+          alteration in some form, by injected humour, or randomised words which
+          don't look even slightly believable. If you are going to use a passage
+          of Lorem Ipsum, you need to be sure there isn't anything embarrassing
+          hidden in the middle of text. All the Lorem Ipsum generators on the
+          Internet tend to repeat predefined chunks as necessary, making this
+          the first true generator on the Internet. It uses a dictionary of over
+          200 Latin words, combined with a handful of model sentence structures,
+          to generate Lorem Ipsum which looks reasonable. The generated Lorem
+          Ipsum is therefore always free from repetition, injected humour, or
+          non-characteristic words etc. 5 paragraphs words bytes lists Start
+          with 'Lorem ipsum dolor sit amet...' Lorem Ipsum is simply dummy text
+          of the printing and typesetting industry. Lorem Ipsum has been the
+          industry's standard dummy text ever since the 1500s,
         </p>
       </Modal>
     </>
