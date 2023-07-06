44dcdb79b611e4c21d1e89cff097fc3a029c1280
gunjan.agarwal <gunjan.agarwal@thoughtworks.com>
2023-07-06 21:45:05 +0530

    [Gunjan/Dhruva] Refactor. Login Component

---
+++ src/utils/User.ts
@@ -0,0 +1,5 @@
+import { User } from '../types/Interfaces';
+export const USERS: User[] = [
+  { id: '1', username: 'user', password: 'User@123', profile: 'user' },
+  { id: '999', username: 'admin', password: 'Admin@123', profile: 'admin' },
+];
